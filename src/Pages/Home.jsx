7889b7d8f51eeab36b639f3e9a2652d42e968b6f
NPATE170 <nispat203@gmail.com>
2023-10-18 21:42:26 +0100

    undoing changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,22 +1,11 @@
 import React from "react";
 import Banner from "../Components/Banner/Banner";
-import InfoColumn from "../Components/InfoColumn/InfoColumn";
 
 export default function Home() {
     return (
         <div className="homeContent">
             <div className="bannerContent">
                 <Banner />
-            </div>
-            <div className="infoColumnContent">
-                <InfoColumn
-                    src={require("../Images/banner-bg-small.jpeg")}
-                    alt="test"
-                    text="Hello world 1" />
-                <InfoColumn
-                    src={require("../Images/banner-bg-small.jpeg")}
-                    alt="test"
-                    text="Hello world 2" />
             </div>
         </div>
     )
